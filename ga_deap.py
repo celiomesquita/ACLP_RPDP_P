34246ae0a8c2f3352f8fe9e66eb1c87d2251bced
Antonio Celio Pereira de Mesquita <celiomap@gmail.com>
2024-01-27 11:47:31 -0300

    sOME NOTEBOOKS INCLUDED

--- ga_deap.py
+++ ga_deap.py
@@ -1,59 +1,87 @@
 import numpy as np
 from deap import base, creator, tools, algorithms
+import time
+# import pandas as pd
 
-# Example MKP data
-# Values of items
-values = np.array([60, 100, 120]) # scores
+def ga_deap(items_file, base_index):
 
-# Weights of items in each dimension
-weights = np.array([[10, 20, 30],  # Dimension Volume)
-                    [20, 15, 25]]) # Dimension Weight)
+    items_matrix_np = np.loadtxt(items_file)
 
-# Capacities of the knapsack in each dimension
-#                      m3   kg
-capacities = np.array([50, 50]) # pallets capacities
+    # filter items on the base
+    items_matrix_np = items_matrix_np[((items_matrix_np[:,3] == base_index))]
 
-# Genetic Algorithm setup                          (volumes, weights)
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+    items_weights = items_matrix_np[ :,0]
+    items_scores  = items_matrix_np[ :,1]
+    items_volumes = items_matrix_np[ :,2]
 
-toolbox = base.Toolbox()
-toolbox.register("attr_bool", np.random.randint, 0, 2)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=len(values))
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    # Weights of items in each dimension
+    weights = np.array([items_volumes,  # Dimension Volume)
+                        items_weights]) # Dimension Weight)
 
-def evalMKP(individual):
-    weight_tot = np.dot(weights, individual)
-    value_tot = np.dot(values, individual)
+    values = items_scores
 
-    # Check if the weight is greater than the capacity for any dimension
-    if any(weight_tot > capacities):
-        return 0,  # Return 0 if the individual exceeds capacity constraints
+    # pallets capacities    m3   kg
+    capacities = np.array([14.8, 4500])
 
-    return value_tot,
+    # Genetic Algorithm setup                          (volumes, weights)
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
 
-toolbox.register("evaluate", evalMKP)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox = base.Toolbox()
+    toolbox.register("attr_bool", np.random.randint, 0, 2)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=len(values))
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-# Genetic Algorithm parameters
-population_size = 50
-crossover_probability = 0.7
-mutation_probability = 0.2
-number_of_generations = 100
+    def evalMKP(individual):
+        weight_tot = np.dot(weights, individual)
+        value_tot = np.dot(values, individual)
 
-# Solve the MKP
-population = toolbox.population(n=population_size)
+        # Check if the weight is greater than the capacity for any dimension
+        if any(weight_tot > capacities):
+            return 0,  # Return 0 if the individual exceeds capacity constraints
 
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", np.mean)
-stats.register("max", np.max)
+        return value_tot,
 
-best = tools.HallOfFame(1)
+    toolbox.register("evaluate", evalMKP)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
 
-algorithms.eaSimple(population, toolbox, cxpb=crossover_probability, mutpb=mutation_probability, 
-                    ngen=number_of_generations, stats=stats, halloffame=best, verbose=False)
+    # Genetic Algorithm parameters
+    population_size = 500
+    crossover_probability = 0.7
+    mutation_probability = 0.05
+    number_of_generations = 1500
 
-# Best solution
-print(f"Best Individual = {best[0]}, Value = {best[0].fitness.values[0]}")
+    # Solve the MKP
+    population = toolbox.population(n=population_size)
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", np.mean)
+    stats.register("max", np.max)
+
+    best = tools.HallOfFame(1)
+
+    algorithms.eaSimple(population, toolbox, cxpb=crossover_probability, mutpb=mutation_probability, 
+                        ngen=number_of_generations, stats=stats, halloffame=best, verbose=False)
+    
+    return best
+
+if __name__ == "__main__":
+
+    startTime = time.perf_counter()
+
+    # items: w, s, v, node, to
+    items_file = "./surplus20/scenario_2/instance_1/items.txt"  
+
+    base_index = 0  
+
+    best = ga_deap(items_file, base_index)
+
+    # Best solution
+    print(f"Best Individual = {best[0]} ({len(best[0])} items), Value = {best[0].fitness.values[0]}")
+
+    elapsed = time.perf_counter() - startTime
+    print(f"{elapsed:.1f} seconds ")
+
+    # print("----- Please execute the main py file -------------") 
